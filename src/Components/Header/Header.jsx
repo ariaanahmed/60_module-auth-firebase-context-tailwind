d450f278c0678892940eca7a9544b77b033e4d13
ariaan ahmed <you@example.com>
2023-04-19 14:42:07 +0600

    60-2 Create Login and Registration Form using Daisy UI

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,9 +6,9 @@ const Header = () => {
         <div className="navbar bg-primary text-primary-content justify-between w-12/12 mx-auto px-5">
             <Link className="normal-case text-xl" to='/'>AuthMaster</Link>
             <div className='flex gap-4'>
-                <Link to='/'>Home</Link>
-                <Link to='/login'>Login</Link>
-                <Link to='/register'>Register</Link>
+                <Link className="tooltip tooltip-bottom" data-tip="Home" to='/'>Home</Link>
+                <Link className="tooltip tooltip-bottom" data-tip="Login" to='/login'>Login</Link>
+                <Link className="tooltip tooltip-bottom" data-tip="Register" to='/register'>Register</Link>
             </div>
         </div>
     );
